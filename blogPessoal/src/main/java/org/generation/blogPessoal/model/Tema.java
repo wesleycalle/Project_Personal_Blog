71d3bf0b23aa8c0db091bced91b21b4b94d2cf3e
wesleycalle <wesleyninaja@gmail.com>
2021-12-15 14:50:16 -0300

    @WesNinaja [Task: Change Tema Model]

---
+++ blogPessoal/src/main/java/org/generation/blogPessoal/model/Tema.java
@@ -0,0 +1,58 @@
+package org.generation.blogPessoal.model;
+
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@Table (name = "tb_tema")
+public class Tema {
+	
+	@Id
+	@GeneratedValue (strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@NotBlank (message = "Coloque um atributo, por favor.")
+	private String descricao;
+	
+	@OneToMany(mappedBy = "tema", cascade= CascadeType.REMOVE)
+	@JsonIgnoreProperties("tema")
+	private List<Postagem> postagem;
+	
+	
+	//GETTERS E SETTERS
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public List<Postagem> getPostagem() {
+		return postagem;
+	}
+
+	public void setPostagem(List<Postagem> postagem) {
+		this.postagem = postagem;
+	}
+
+	
+}

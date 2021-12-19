93b2c5035aadaf0f39619e8f1f1aaa04e6733563
wesleycalle <wesleyninaja@gmail.com>
2021-12-19 20:17:15 -0300

    @WesNinaja [Task: Modify Postagem Model]

--- blogPessoal/src/main/java/org/generation/blogPessoal/model/Postagem.java
+++ blogPessoal/src/main/java/org/generation/blogPessoal/model/Postagem.java
@@ -1,6 +1,6 @@
 package org.generation.blogPessoal.model;
 
-import java.util.Date;
+import java.time.LocalDateTime;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
@@ -10,72 +10,78 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
-
 @Entity
-@Table (name= "postagem")
+@Table(name = "postagem")
 public class Postagem {
-	
-	//ATRIBUTOS
-	
+
+	// ATRIBUTOS
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
-	
+
 	@NotBlank
-	@Size (min=5, max=100)
+	@Size(min = 5, max = 100)
 	private String titulo;
-	
+
 	@NotBlank
-	@Size (min=10, max=500)
+	@Size(min = 10, max = 500)
 	private String texto;
-	
-	@Temporal(TemporalType.TIMESTAMP)
-	private Date date = new java.sql.Date(System.currentTimeMillis());
-	
-	@ManyToOne(cascade= CascadeType.REMOVE)
-	@JoinColumn(name= "fk_tema")
+
+	@JsonFormat(pattern = "dd/MM/yyyy HH:mm:ss")
+	private LocalDateTime data = LocalDateTime.now();
+
+	@ManyToOne(cascade = CascadeType.REMOVE)
+	@JoinColumn(name = "fk_tema")
 	@JsonIgnoreProperties("postagem")
 	private Tema tema;
-	
-	//GETTERS E SETTERS
-	
+
+	// GETTERS E SETTERS
+
 	public long getId() {
 		return id;
 	}
+
 	public void setId(long id) {
 		this.id = id;
 	}
+
 	public String getTitulo() {
 		return titulo;
 	}
+
 	public void setTitulo(String titulo) {
 		this.titulo = titulo;
 	}
+
 	public String getTexto() {
 		return texto;
 	}
+
 	public void setTexto(String texto) {
 		this.texto = texto;
 	}
-	public Date getDate() {
-		return date;
+
+	public LocalDateTime getData() {
+		return data;
 	}
-	public void setDate(Date date) {
-		this.date = date;
+
+	public void setData(LocalDateTime data) {
+		this.data = data;
 	}
-	
+
 	public Tema getTema() {
 		return tema;
 	}
+
 	public void setTema(Tema tema) {
 		this.tema = tema;
 	}
-	
+
 }

545fc7e7faf204bc04493cf6c3be28b10194abe9
wesleycalle <wesleyninaja@gmail.com>
2021-12-20 11:21:50 -0300

    @WesNinaja [Task: Modify User Model]

--- blogPessoal/src/main/java/org/generation/blogPessoal/model/Usuario.java
+++ blogPessoal/src/main/java/org/generation/blogPessoal/model/Usuario.java
@@ -1,12 +1,20 @@
 package org.generation.blogPessoal.model;
 
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import io.swagger.v3.oas.annotations.media.Schema;
 
 @Entity
 @Table(name = "tb_usuario")
@@ -20,6 +28,7 @@ public class Usuario {
 	@Size(min = 2, max = 100)
 	private String nome;
 
+	@Schema(example = "email@email.com.br")
 	@NotBlank
 	@Size(min = 5, max = 100)
 	private String usuario;
@@ -28,14 +37,14 @@ public class Usuario {
 	@Size(min = 5, max = 100)
 	private String senha;
 	
+
+	@OneToMany(mappedBy = "usuario", cascade = CascadeType.REMOVE)
+	@JsonIgnoreProperties("usuario")
+	private List<Postagem> postagem;
+	
 	// CONSTRUTORES
 	
-	public Usuario() {}
-
-
-	
-
-	
+	public Usuario() {}	
 
 	public Usuario(long id, String nome, String usuario, String senha) {
 		this.id = id;
@@ -43,63 +52,47 @@ public class Usuario {
 		this.usuario = usuario;
 		this.senha = senha;
 	}
-
+	
 	// GETTERS AND SETTERS
-
 
 	public long getId() {
 		return id;
 	}
 
-
-
-
 	public void setId(long id) {
 		this.id = id;
 	}
-
-
-
 
 	public String getNome() {
 		return nome;
 	}
 
-
-
-
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
-
-
-
 
 	public String getUsuario() {
 		return usuario;
 	}
 
-
-
-
 	public void setUsuario(String usuario) {
 		this.usuario = usuario;
 	}
-
-
-
 
 	public String getSenha() {
 		return senha;
 	}
 
-
-
-
 	public void setSenha(String senha) {
 		this.senha = senha;
 	}
 
+	public List<Postagem> getPostagem() {
+		return postagem;
+	}
 
-
+	public void setPostagem(List<Postagem> postagem) {
+		this.postagem = postagem;
+	}
+	
 }

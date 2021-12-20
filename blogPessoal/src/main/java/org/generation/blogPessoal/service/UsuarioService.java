61db5077aaabfc67fc04282d494a239a2d8843ec
wesleycalle <wesleyninaja@gmail.com>
2021-12-20 08:05:34 -0300

    @WesNinaja [Task: Modify Usuario Service]

--- blogPessoal/src/main/java/org/generation/blogPessoal/service/UsuarioService.java
+++ blogPessoal/src/main/java/org/generation/blogPessoal/service/UsuarioService.java
@@ -8,8 +8,10 @@ import org.generation.blogPessoal.model.UserLogin;
 import org.generation.blogPessoal.model.Usuario;
 import org.generation.blogPessoal.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 @Service
 public class UsuarioService {
@@ -19,18 +21,48 @@ public class UsuarioService {
 
 	// CRIAÇÃO DA REGRA DE NEGÓCIO PARA CADASTRAR USUÁRIO
 
-	public Usuario cadastrarUsuario(Usuario usuario) {
+	public Optional<Usuario> cadastrarUsuario(Usuario usuario) {
+
+		if (repository.findByUsuario(usuario.getUsuario()).isPresent())
+			return Optional.empty();
+
+		usuario.setSenha(criptografarSenha(usuario.getSenha()));
+
+		return Optional.of(repository.save(usuario));
+
+	}
+
+	// CRIPTOGRAFAR SENHA
+	private String criptografarSenha(String senha) {
+
 		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
 
-		String senhaEncoder = encoder.encode(usuario.getSenha());
-		usuario.setSenha(senhaEncoder);
+		return encoder.encode(senha);
+	}
 
-		return repository.save(usuario);
+	// ATUALIZAR USUARIO
+	public Optional<Usuario> atualizarUsuario(Usuario usuario) {
+
+		if (repository.findById(usuario.getId()).isPresent()) {
+
+			Optional<Usuario> buscaUsuario = repository.findByUsuario(usuario.getUsuario());
+
+			if ((buscaUsuario.isPresent()) && (buscaUsuario.get().getId() != usuario.getId()))
+				throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Usuário já existe!", null);
+
+			usuario.setSenha(criptografarSenha(usuario.getSenha()));
+
+			return Optional.ofNullable(repository.save(usuario));
+
+		}
+
+		return Optional.empty();
+
 	}
 
 	// REGRA DE NEGÓCIO QUE VAI DITAR TUDO QUE SE REFERE A LOGAR
 	public Optional<UserLogin> logar(Optional<UserLogin> user) {
-		
+
 		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
 		Optional<Usuario> usuario = repository.findByUsuario(user.get().getUsuario());
 

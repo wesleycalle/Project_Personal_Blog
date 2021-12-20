ab2e7a44303bb915b2918cda730db9cf11a453d2
wesleycalle <wesleyninaja@gmail.com>
2021-12-20 08:06:01 -0300

    @WesNinaja [Task: Modify Usuario Model]

--- blogPessoal/src/main/java/org/generation/blogPessoal/model/Usuario.java
+++ blogPessoal/src/main/java/org/generation/blogPessoal/model/Usuario.java
@@ -33,7 +33,12 @@ public class Usuario {
 	public Usuario() {}
 
 
-	public Usuario(String nome, String usuario, String senha) {
+	
+
+	
+
+	public Usuario(long id, String nome, String usuario, String senha) {
+		this.id = id;
 		this.nome = nome;
 		this.usuario = usuario;
 		this.senha = senha;
@@ -41,35 +46,60 @@ public class Usuario {
 
 	// GETTERS AND SETTERS
 
+
 	public long getId() {
 		return id;
 	}
+
+
+
 
 	public void setId(long id) {
 		this.id = id;
 	}
 
+
+
+
 	public String getNome() {
 		return nome;
 	}
+
+
+
 
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
 
+
+
+
 	public String getUsuario() {
 		return usuario;
 	}
+
+
+
 
 	public void setUsuario(String usuario) {
 		this.usuario = usuario;
 	}
 
+
+
+
 	public String getSenha() {
 		return senha;
 	}
 
+
+
+
 	public void setSenha(String senha) {
 		this.senha = senha;
 	}
+
+
+
 }
